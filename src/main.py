f5f0b0f996559ea0a42bc652f836cf7750f9a314
jahid15 <ibnasinha15@gmail.com>
2026-06-13 23:14:01 +0600

    Add basic calculator functions in utils.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 from datetime import date
-from src.utils import add, subtract
-
+from utils import add, subtract
 
 print("My name is: Jahid Ibna Sinha")
 print("Today's date is:", date.today())
 
-
+print("\n--- Calculator Results ---")
 print("5 + 3 =", add(5, 3))
 print("10 - 4 =", subtract(10, 4))
